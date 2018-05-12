3382d021b22e26799b5c1edf68ae4b0817520446
cprice <cprice@tutanota.com>
2018-05-12 19:40:46 +0200

    [service-name] implement env var resolving

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import AWS from 'aws-sdk'
 
 const lambda = new AWS.Lambda()
 
-const resolveStage = () => undefined        // TODO, we want stage to be deduced from current env. ENV_VAR ?
-const resolveServiceName = () => undefined  // TODO, we want to call a lambda in the SAME service. ENV_VAR ?
+const resolveServiceName = () =>
+  `${process.env.SLS_SERVICE_NAME}-${process.env.SLS_STAGE}`
 
 export default async (FunctionName, Payload) => {
   const lambdaReturn = await lambda
     .invoke({
-      FunctionName: `${resolveServiceName}-${resolveStage}-${FunctionName}`,
+      FunctionName: `${resolveServiceName()}-${FunctionName}`,
       Payload: JSON.stringify(Payload)
     })
     .promise()
     .catch(err => {
-      console.error(`Inner lambda call failed (${FunctionName})`, err)
+      console.error(`[lambda-execute] - Inner lambda call failed (${FunctionName})`, err)
       throw err
     })
   return JSON.parse(lambdaReturn.Payload)
